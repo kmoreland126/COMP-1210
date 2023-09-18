cc230078e9c1bba849386030328f36ce36502ff9
kmoreland126 <142517368+kmoreland126@users.noreply.github.com>
2023-09-17 23:20:40 -0500

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,20 @@
+/**
+* Simple program to give information about Student.
+*
+* Activity 1
+* @author Kate Moreland - Fundamentals of Computing 1 - 001
+* @version 1-15-23
+*/
+public class StudentInfo 
+{
+/**
+* Prints student's name and previous computer courses that have been taken.
+* @param args Command line arguments - not used.
+*/
+   public static void main(String[] args)
+   {
+      System.out.println("Name: Kate Moreland");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   None");
+   }
+}
